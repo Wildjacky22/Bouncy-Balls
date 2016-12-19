c110f29243264e5e3c8bd3a9b2d203104904b6ec
Wildjacky22 <62379@bwls.net>
2016-12-19 11:46:11 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,129 +1,69 @@
+ var N = 25;
+
+var B1;
+var B2;
+
+
 function setup() {
   createCanvas( windowWidth , windowHeight );
-
-  noStroke();
   
-  background( 0 , 0 , 0 )
+  B1 = new Array(N);
+  B2 = new Array(N);
+
+  for ( n = 0 ; n < N ; n++ ){
+    B1[n] = new Ball();
+    B2[n] = new Ball();
+
+    }
+  
+  stroke( 0 , 0 , 0 );
+  //noStroke();
 }
+
 function draw() {
-  
 
-  textSize(60);
-  fill( 0 , 255 , 0 );
-  text("Touch screen or move mouse to draw." , 150 , 300 );
- 
-  var r = random( 0 , 100 );
-  var g = random( 0 , 250 );
-  var b = random( 0 , 255 );
-  var a = random( 0 , 100 );
+  for ( n = 0 ; n < N ; n++ ){
+    B1[n].evolveDraw();
+    B2[n].evolveDraw();
+    
+    
+  }
+}
 
-  ellipse( mouseX , mouseY , 20 , 20 );
-   fill( r , g , b , a );
-  
-  ellipse( mouseX , mouseY , 30 , 30 );
-    fill( r , g , b , a );
-  
-  ellipse( mouseX , mouseY , 40 , 40);
-    fill( r , g , b , a );
 
-  ellipse( mouseX , mouseY , 50 , 50);
-    fill( r , g , b , a );
- 
- // ellipse( mouseX , mouseY , 55 , 55);
-   // fill( r , g , b , a );
-}  
-  //---------------------------------
+var Ball = function(){
+  this.pos = createVector( width/2 , height/2 );
   
-  //var r = random( 33 , 100 );
- // var g = random( 0 , 120 );
-  //var b = random( 155 , 255);
+  this.v = p5.Vector.random2D();
+   this.v.mult( random( 1 , 3 ) );
   
-// ellipse( mouseX , mouseY , 2 , 2 );
-   // fill( r , g , b , 100);
+  this.evolveDraw =function() {
+    this.pos.add( this.v );
   
- //ellipse( mouseX , mouseY , 75 , 75 );
-   // fill( r , g , b , 100 );
-   
- // ellipse( mouseX , mouseY , 70 , 70 );
-    //fill( r , g , b , 100 );
+    if ( this.pos.x >= width || this.pos.x <= 0 ){
+      this.v.x *=-1;
+    }
+    if ( this.pos.y >= height || this.pos.y <= 0 ){
+      this.v.y *=-1;
+    }
     
-  //ellipse( mouseX , mouseY , 65 , 65 );
-    //fill( r , g , b , 100 );
+    var r=( 0 || 255 );
+    var g=( 0 || 255 );
+    var b=( 0 || 255 );
     
-  //ellipse( mouseX , mouseY , 60 , 60 );
-    //fill( r , g , b , 100 );
     
-  //ellipse( mouseX , mouseY , 55 , 55 );
-    //fill( r , g , b , 100 );
     
-  //ellipse( mouseX , mouseY , 50 , 50 );
-    //fill( r , g , b , 100 );
+    fill( r , g , b || r , g ,b );
+    ellipse( this.pos.x , this.pos.y , 20 );
+    
+  };
   
-  //ellipse( mouseX , mouseY , 45 , 45 );
-    //fill( r , g , b , 100 );
-    
-  //ellipse( mouseX , mouseY , 40 , 40 );
-   // fill( r , g , b , 100 );
-    
- // ellipse( mouseX , mouseY , 35 , 35 );
-   // fill( r , g , b , 100 );
-    
- // ellipse( mouseX , mouseY , 30 , 30 );
-    //fill( r , g , b , 100 );
-    
-  //ellipse( mouseX , mouseY , 25 , 25 );
-//   fill( r , g , b , 100 );
-  
-  //ellipse( mouseX , mouseY , 20 , 20 );
-   //fill( r , g , b , 100);
+};
 
-//  ellipse( mouseX , mouseY , 15 , 15 );
-  //  fill( r , g , b , 100 );
-  
-// -------------------------  
-  
- //ellipse( mouseX , mouseY , 2 , 2 );
-    //fill( 66 , 33 , 240 , 100);
-  
- //ellipse( mouseX , mouseY , 75 , 75 );
-   // fill( 66 , 44 , 240 , 100 );
-   
-  //ellipse( mouseX , mouseY , 70 , 70 );
-    //fill( 66 , 45 , 250 , 100 );
-    
-  //ellipse( mouseX , mouseY , 65 , 65 );
-    //fill( 66 , 49 , 249 , 100 );
-    
-  //ellipse( mouseX , mouseY , 60 , 60 );
-    //fill( 66 , 55 , 240 , 100 );
-    
-  //ellipse( mouseX , mouseY , 55 , 55 );
-    //fill( 66 , 66 , 244 , 100 );
-    
-  //ellipse( mouseX , mouseY , 50 , 50 );
-    //fill( 65 , 83 , 244 , 100 );
-  
-  //ellipse( mouseX , mouseY , 45 , 45 );
-    //fill( 66 , 116 , 244 , 100 );
-    
-  //ellipse( mouseX , mouseY , 40 , 40 );
-    //fill( 66 , 138 , 244 , 100 );
-    
-  //ellipse( mouseX , mouseY , 35 , 35 );
-    //fill( 66 , 153 , 255 , 100 );
-    
-  //ellipse( mouseX , mouseY , 30 , 30 );
-    //fill( 66 , 177 , 244 , 100 );
-    
-  //ellipse( mouseX , mouseY , 25 , 25 );
-   //fill( 66 , 192 , 244 , 100 );
-  
-  //ellipse( mouseX , mouseY , 20 , 20 );
-   //fill( 66 , 212 , 244 , 100);
 
-  //ellipse( mouseX , mouseY , 15 , 15 );
-    //fill( 66 , 233 , 255 , 100 );
-
-  
-    
-
+function keyTyped() {
+  if( key === 's' ) {
+    saveCanvas( 'canvas' , 'jpg' );
+    console.log("saved");
+  }
+}

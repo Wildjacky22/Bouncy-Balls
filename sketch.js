0eaf952c11cf907f45c760a71a961cc3f0fe54f1
Wildjacky22 <62379@bwls.net>
2017-01-03 11:33:51 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -100,7 +100,7 @@ var Ball = function(){
   var b = random( 0 || 255);
 
    
-    fill( 0||255 , 0||255 , 0||255 );
+    fill random( 0||255 , 0||255 , 0||255 );
     ellipse( this.pos.x , this.pos.y , 20 );
    
     
